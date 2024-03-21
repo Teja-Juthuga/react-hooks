6341b0baa571e0ba51a78e067e2fd85860ee45fc
Teja Juthuga <tejajuthuga@gmail.com>
2024-03-21 21:52:52 +0530

    Developed Products UI in fakeStore API

--- src/components/useEffect/fakestore/fakestore.jsx
+++ src/components/useEffect/fakestore/fakestore.jsx
@@ -1,47 +1,89 @@
-import {useState, useEffect} from "react";
+import { useState, useEffect } from "react";
 
-export function Shopper(){
-
+export function Shopper() {
     const [categories, setCategories] = useState([""]);
-    
+
+    const [products, setProducts] = useState([
+        {
+            id: 0,
+            title: "",
+            price: 0,
+            description: "",
+            category: "",
+            image: "",
+            rating: {
+                rate: 0,
+                count: 0,
+            },
+        },
+    ]);
+
     function capitalizeFirstLetter(string) {
         return string.charAt(0).toUpperCase() + string.slice(1);
     }
 
+    function loadProducts() {
+        fetch("https://fakestoreapi.com/products")
+            .then((response) => response.json())
+            .then((products_obj) => setProducts(products_obj));
+    }
+
     useEffect(() => {
         fetch("https://fakestoreapi.com/products/categories")
-        .then(res => res.json())
-        .then(categories_obj => setCategories(categories_obj))
-    }, [])
+            .then((res) => res.json())
+            .then((categories_obj) => setCategories(categories_obj));
+
+        loadProducts();
+    }, []);
 
     return (
-        <div className="w-100" style={{height:"100vh"}}>
-            <nav className="p-3 bg-dark text-light d-flex justify-content-between">
+        <div >
+            <nav className="p-3 bg-dark text-light d-flex justify-content-around">
                 <h4> Shopper. </h4>
                 <div>
-                    {
-                        categories.map(category => 
-                            <a className="me-2" key={category}>{capitalizeFirstLetter(category)} </a>    
-                        )
-                    }
+                    {categories.map((category) => (
+                        <a className="me-2" key={category}>
+                            {capitalizeFirstLetter(category)}{" "}
+                        </a>
+                    ))}
                 </div>
-                <button className="btn btn-light"> Cart <span className="bi bi-cart4"></span> </button>
+                <button className="btn btn-light">
+                    {" "}
+                    Cart <span className="bi bi-cart4"></span>{" "}
+                </button>
             </nav>
             <main>
                 <div className="row">
-                    <div className="col-9">
+                    <div className="col-10">
                         <div className="d-flex flex-wrap">
-                            <div className="card">
-                                
-                            </div>
+                            {products.map((product) => (
+                                <div
+                                    class="card m-3 p-3"
+                                    style={{ width: "18rem" }}
+                                >
+                                    <div className="h5 text-center p-2">
+                                        {" "}
+                                        {product.title}{" "}
+                                    </div>
+                                    <div className="text-center">
+                                        <img
+                                            src={product.image}
+                                            class="card-img-top "
+                                        />
+                                    </div>
+                                    <div class="card-body">
+                                        <p class="card-text">
+                                            {product.description}
+                                        </p>
+                                    </div>
+                                    <button className="btn btn-danger"> Buy Now </button>
+                                </div>
+                            ))}
                         </div>
                     </div>
-                    <div className="col-3">
-
-                    </div>
-
+                    <div className="col-2 p-0"></div>
                 </div>
             </main>
         </div>
-    )
+    );
 }
